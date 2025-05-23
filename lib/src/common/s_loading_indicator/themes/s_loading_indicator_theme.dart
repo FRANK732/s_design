6e8536b272cb9ed352af238dc3e734c66830c772
FRANK732 <hacphran122@gmail.com>
2025-05-23 12:01:44 +0000

    feat: enhance SLoadingIndicatorThemeData with default dark theme and improved properties

--- lib/src/common/s_loading_indicator/themes/s_loading_indicator_theme.dart
+++ lib/src/common/s_loading_indicator/themes/s_loading_indicator_theme.dart
@@ -3,6 +3,46 @@ import 'package:flutter/material.dart';
 /// Theme data for [SLoadingIndicator].
 @immutable
 class SLoadingIndicatorThemeData {
+  /// Provides a default dark theme.
+  factory SLoadingIndicatorThemeData.dark() {
+    return SLoadingIndicatorThemeData(
+      spinnerColor: Colors.white,
+      messageStyle: const TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.white70,
+      ),
+      backgroundColor: Colors.grey[900]!,
+      shadowColor: Colors.black54,
+    );
+  }
+
+  /// Provides a default light theme.
+  factory SLoadingIndicatorThemeData.light() {
+    return const SLoadingIndicatorThemeData();
+  }
+
+  const SLoadingIndicatorThemeData({
+    this.spinnerColor = Colors.teal,
+    this.spinnerSize = 25.0,
+    this.messageStyle = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      color: Colors.black87,
+    ),
+    this.useGradient = false,
+    this.gradient,
+    this.backgroundColor = Colors.white,
+    this.shadowColor = Colors.black26,
+    this.shadowBlurRadius = 10.0,
+    this.shadowOffset = const Offset(0, 4),
+    this.animationCurve = Curves.easeInOut,
+    this.spinnerSpeed = 1.0,
+    this.padding = const EdgeInsets.all(20.0),
+    this.margin = EdgeInsets.zero,
+    this.borderRadius = const BorderRadius.all(Radius.circular(20)),
+  });
+
   /// Default color for the loading spinner.
   final Color spinnerColor;
 
@@ -45,47 +85,6 @@ class SLoadingIndicatorThemeData {
   /// Border radius of the loading indicator container.
   final BorderRadiusGeometry borderRadius;
 
-  /// Creates an instance of [SLoadingIndicatorThemeData].
-  const SLoadingIndicatorThemeData({
-    this.spinnerColor = Colors.teal,
-    this.spinnerSize = 25.0,
-    this.messageStyle = const TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.w500,
-      color: Colors.black87,
-    ),
-    this.useGradient = false,
-    this.gradient,
-    this.backgroundColor = Colors.white,
-    this.shadowColor = Colors.black26,
-    this.shadowBlurRadius = 10.0,
-    this.shadowOffset = const Offset(0, 4),
-    this.animationCurve = Curves.easeInOut,
-    this.spinnerSpeed = 1.0,
-    this.padding = const EdgeInsets.all(20.0),
-    this.margin = EdgeInsets.zero,
-    this.borderRadius = const BorderRadius.all(Radius.circular(20)),
-  });
-
-  /// Provides a default light theme.
-  factory SLoadingIndicatorThemeData.light() {
-    return const SLoadingIndicatorThemeData();
-  }
-
-  /// Provides a default dark theme.
-  factory SLoadingIndicatorThemeData.dark() {
-    return SLoadingIndicatorThemeData(
-      spinnerColor: Colors.white,
-      messageStyle: const TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.w500,
-        color: Colors.white70,
-      ),
-      backgroundColor: Colors.grey[900]!,
-      shadowColor: Colors.black54,
-    );
-  }
-
   /// Creates a copy of this theme with the given fields replaced with the new values.
   SLoadingIndicatorThemeData copyWith({
     Color? spinnerColor,
@@ -127,5 +126,5 @@ extension SLoadingIndicatorThemeExtension on ThemeData {
   /// Retrieves the current [SLoadingIndicatorThemeData].
   SLoadingIndicatorThemeData get sLoadingIndicatorTheme =>
       extension<SLoadingIndicatorThemeData>() ??
-           SLoadingIndicatorThemeData.light();
+      SLoadingIndicatorThemeData.light();
 }
